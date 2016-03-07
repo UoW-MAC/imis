40bea7232607dec9675271492a4c3682e10be593
chentongboy <chentong0211@gmail.com>
2016-03-07 14:57:32 -0500

    删除:         src/main/java/com/imis/domain/repositories/ApplicationRepository.java 	删除:         src/main/webapp/js/app/modules/application.js 	删除:         src/main/webapp/pages/position-detail.jsp

---
+++ src/main/java/com/imis/domain/repositories/ApplicationRepository.java
@@ -0,0 +1,21 @@
+package com.imis.domain.repositories;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.imis.domain.entities.Application;
+
+/**
+ * @author william zhang
+ * 
+ */
+@Repository
+@Transactional
+public interface ApplicationRepository {
+	
+	void positionApply(Application application); 
+	void acceptCandidate(int applicationId);
+	void rejectCandidate(int applicationId);
+}
